111de153d15d0fc4d544c2e4bf20c0f8f953138b
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-11-08 11:44:14 -0600

    changes some file structure

--- api/server.js
+++ api/server.js
@@ -2,13 +2,13 @@ const express = require('express')
 const server = express()
 server.use(express.json())
 
-const projects = require('./routes/projects.js')
+const projects = require('../routes/projects.js')
 server.use('/api/projects', projects)
 
-const tasks = require('./routes/tasks.js')
+const tasks = require('../routes/tasks.js')
 server.use('/api/tasks', tasks)
 
-const resources = require('./routes/resources.js')
+const resources = require('../routes/resources.js')
 server.use('/api/resources', resources)
 
 server.get('/', (req, res) => {
---
+++ models/tasks-model.js
@@ -0,0 +1,4 @@
+const db = require('../data/dbconfig.js')
+module.exports = {
+    
+}
