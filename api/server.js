29a094f414fcb059d04442ad45d82b7b51e9e924
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-11-08 11:39:02 -0600

    added more files

--- api/server.js
+++ api/server.js
@@ -8,6 +8,9 @@ server.use('/api/projects', projects)
 const tasks = require('./routes/tasks.js')
 server.use('/api/tasks', tasks)
 
+const resources = require('./routes/resources.js')
+server.use('/api/resources', resources)
+
 server.get('/', (req, res) => {
     res.send(`
       <h2>Node DB Sprint.</h>
